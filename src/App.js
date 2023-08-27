375b476e809f0641648fe78188088fdb972fcae0
mspark109 <minsup109@gmail.com>
2023-08-28 01:50:22 +0900

    UI 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
-  };
-
-  const onClick2 = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d4aa776502ec466481e24e5fcaa9c126',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick2}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
