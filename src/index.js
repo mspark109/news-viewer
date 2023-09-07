27ffe6286f8a24d77d493480f3ca794c7e02e039
mspark109 <minsup109@gmail.com>
2023-09-08 01:53:15 +0900

    BrowserRouter에 basenmae 추가

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
