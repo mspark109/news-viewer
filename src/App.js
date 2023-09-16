ef18341783a701846a9f48c3150f92d848f3c630
mspark109 <minsup109@gmail.com>
2023-09-17 00:57:32 +0900

    BrowserRouter 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from '../node_modules/react-router-dom/dist/index';
 import NewsPage from './pages/NewsPage';
+import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<NewsPage />} />
-      <Route path="/:category?" element={<NewsPage />} />
-    </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<NewsPage />} />
+        <Route path="/:category?" element={<NewsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
