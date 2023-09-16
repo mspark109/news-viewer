6c1a137ae18ba140018ebc9d1a8d8726272c4ed6
mspark109 <minsup109@gmail.com>
2023-09-17 01:19:22 +0900

    BrowserRouter HashRouter로 변경

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Route, Routes } from '../node_modules/react-router-dom/dist/index';
+import {
+  Route,
+  Routes,
+  HashRouter,
+} from '../node_modules/react-router-dom/dist/index';
 import NewsPage from './pages/NewsPage';
-import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={<NewsPage />} />
         <Route path="/:category?" element={<NewsPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
